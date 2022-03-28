dd2afbcbc9c234254b9446a6a85df75fd3c612c6
Fatimah Sarwar <fatimahsarwar@Fatimahs-MBP.fios-router.home>
2022-03-28 14:57:04 -0400

    added users model

--- api/server.js
+++ api/server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const helmet = require("helmet");
 const cors = require("cors");
-
+const session = require('express-session');
 /**
   Do what needs to be done to support sessions with the `express-session` package!
   To respect users' privacy, do NOT send them a cookie unless they log in.
@@ -17,10 +17,24 @@ const cors = require("cors");
 
 const server = express();
 
+const sessionConfig = {
+  name: 'kahsbfhjasef',
+  secret: "You'll never guess this password",
+  cookie: {
+    maxAge: 1000 * 60,
+    secure: false,
+    httpOnly: true,
+  },
+  resave: false,
+  saveUninitialized: false,
+};
+
+
+
 server.use(helmet());
 server.use(express.json());
 server.use(cors());
-
+server.use(session(sessionConfig));
 server.get("/", (req, res) => {
   res.json({ api: "up" });
 });
