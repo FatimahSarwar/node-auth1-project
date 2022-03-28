a54c4688d2ffe6174870ef8349ceca8a5f7ff4e0
Fatimah Sarwar <fatimahsarwar@Fatimahs-MBP.fios-router.home>
2022-03-28 15:54:08 -0400

    finished

--- api/auth/auth-middleware.js
+++ api/auth/auth-middleware.js
@@ -1,3 +1,5 @@
+const Users = require('../users/users-model');
+
 /*
   If the user does not have a session saved in the server
 
@@ -6,8 +8,15 @@
     "message": "You shall not pass!"
   }
 */
-function restricted() {
-
+function restricted(req, res, next) {
+  if(req.session.user) {
+    next();
+} else {
+    next({
+        status: 401,
+        message: 'You shall not pass!',
+    });
+}
 }
 
 /*
@@ -18,8 +27,13 @@ function restricted() {
     "message": "Username taken"
   }
 */
-function checkUsernameFree() {
-
+function checkUsernameFree(req, res, next) {
+  let [user] = Users.findBy({ username: req.user.username });
+  if(user != null) {
+      next({  status: 422, message: 'Username taken' });
+  } else {
+      next();
+  }
 }
 
 /*
@@ -30,8 +44,14 @@ function checkUsernameFree() {
     "message": "Invalid credentials"
   }
 */
-function checkUsernameExists() {
-
+function checkUsernameExists(req, res, next) {
+  let [user] =  Users.findBy({ username: req.user.username });
+  if(user == null) {
+      next({  status: 401, message: 'Invalid  credentials' });
+  } else {
+      req.user.hash = user.password;
+      next();
+  }
 }
 
 /*
@@ -42,8 +62,16 @@ function checkUsernameExists() {
     "message": "Password must be longer than 3 chars"
   }
 */
-function checkPasswordLength() {
-
+function checkPasswordLength(req,res,next) {
+  const password = req.body.password;
+  if (!password || password.length <3) {
+    res.status(422).json({ message: "Password must be longer than 3 chars" });
+  } else {
+    next();
+  }   
 }
 
 // Don't forget to add these to the `exports` object so they can be required in other modules
+module.exports = {restricted,
+  checkUsernameFree,checkUsernameExists,checkPasswordLength
+};
--- api/auth/auth-router.js
+++ api/auth/auth-router.js
@@ -1,7 +1,11 @@
 // Require `checkUsernameFree`, `checkUsernameExists` and `checkPasswordLength`
 // middleware functions from `auth-middleware.js`. You will need them here!
+const bcrypt = require('bcryptjs');
 
+const router = require("express").Router();
 
+const { checkUsernameFree,checkUsernameExists,checkPasswordLength} = require('./auth-middleware')
+const Users = require('../users/users-model')
 /**
   1 [POST] /api/auth/register { "username": "sue", "password": "1234" }
 
@@ -24,7 +28,18 @@
     "message": "Password must be longer than 3 chars"
   }
  */
-
+  router.post('/register', checkUsernameFree, checkPasswordLength, async (req, res, next) => {
+    try {
+        const hash = bcrypt.hashSync(req.user.password, 8);
+        const user = await Users.add({ username: req.user.username, password: hash });
+        res.status(201).json({
+          user_id: user.user_id,
+          username: user.username,
+        });
+    } catch(err) {
+        next({message: err.message});
+    }
+});
 
 /**
   2 [POST] /api/auth/login { "username": "sue", "password": "1234" }
@@ -41,7 +56,17 @@
     "message": "Invalid credentials"
   }
  */
-
+  router.post("/login",  checkUsernameExists, (req, res, next) => {
+    if(bcrypt.compareSync(req.user.password, req.user.hash)) {
+        req.session.user = {
+            username: req.user.username,
+            password: req.user.hash,
+        };
+        res.status(200).json(`Welcome ${req.user.username}`);
+    } else {
+        next({  status: 400, message: 'invalid  credentials' });
+    }
+});
 
 /**
   3 [GET] /api/auth/logout
@@ -58,6 +83,21 @@
     "message": "no session"
   }
  */
-
+  router.get('/logout', (req, res, next) => {
+    if(req.session.user) {
+       
+        
+        req.session.destroy(err => {
+            if(err) {
+                next({ message: 'error while logging out' });
+            } else {
+                res.json(`logged out`);
+            }
+        })
+    } else {
+        res.json('no session');
+    }
+});
  
 // Don't forget to add the router to the `exports` object so it can be required in other modules
+module.exports = router;
